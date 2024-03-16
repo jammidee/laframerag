5426a4b800760c97ab2b6c0fe4bff6b0402cb71c
Jammi Dee <jammi_dee@yahoo.com>
2024-03-16 18:28:49 +0800

    Added nod-nlp libraries

--- src/index.js
+++ src/index.js
@@ -693,6 +693,16 @@ ipcMain.on('req-ai-answer', async (event, params) => {
 
 });
 
+const { dockStart } = require('@nlpjs/basic');
+
+(async () => {
+  const dock = await dockStart();
+  const nlp = dock.get('nlp');
+  //await nlp.train();
+  const response = await nlp.process('en', 'Who are you');
+  console.log(response);
+})();
+
 //=======================================
 // Listen to all IPC calls and handle it
 //=======================================
